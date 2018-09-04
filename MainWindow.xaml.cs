6ec8248fa4beda6d8a674ca36b9977afd2d696a9
JlongPyDev <jeffrey.long.88@gmail.com>
2018-09-04 11:53:57 -0400

    1st sprint widget functionality
    - still need to get ESRI basemapp displaying properly

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
+using System.Configuration;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -15,37 +15,48 @@ using System.Windows.Shapes;
 using System.Data.SqlClient;
 using System.Data;
 using Esri.ArcGISRuntime.Mapping;
+using System.IO;
+using System.Runtime.CompilerServices;
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Location;
+using Esri.ArcGISRuntime.Security;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.Tasks;
+using Esri.ArcGISRuntime.UI;
+using System.ComponentModel;
 
 namespace CemMapApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
+
     {
         SqlConnection cnn;
         string strConn;
+        string sql1, sql2;
+        string sqllot, cId;
+
+
         public MainWindow()
         {
-            InitializeComponent();
-        }
+            //InitializeComponent();
 
-        private void btnLogin_Click(object sender, EventArgs e)
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                //PROD
                 strConn = "Data Source=ApexGIS;Initial catalog=ApexCEM;User Id=" + Id_TextEntry.Text + ";Password=" + Pw_Box.Password + ";";
 
-                //TEST
-                //strConn = "Data Source=ApexGIS;Initial catalog=Sandbox;User Id=" + txtId.Text + ";Password=" + txtPW.Text + ";";
+
                 cnn = new SqlConnection(strConn);
                 cnn.Open();
-                SearchBtn.IsEnabled = true;
+                Logon_btn.IsEnabled = true;
                 Logoff_btn.IsEnabled = true;
-                Logon_btn.IsEnabled = false;
                 Id_TextEntry.IsEnabled = false;
                 Pw_Box.IsEnabled = false;
+                this.Title = "Logged In";
+
             }
             catch
             {
@@ -54,7 +65,85 @@ namespace CemMapApp
         }
 
 
+
+
+        private void LogOffClose_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cnn.Close();
+                cnn.Dispose();
+                Logoff_btn.IsEnabled = false;
+                SearchBtn.IsEnabled = true;
+                Logon_btn.IsEnabled = true;
+                Id_TextEntry.IsEnabled = true;
+                Pw_Box.IsEnabled = true;
+                Id_TextEntry.Clear();
+                Pw_Box.Clear();
+                LastNameSearch.Clear();
+                PlotSearch.Clear();
+                // this.Close();
+                this.Title = "Logged Out";
+            }
+            catch { this.Close(); }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {//populate the grid based on lot
+
+            //clearData();
+            SearchBtn.IsEnabled = true;
+            sqllot = this.PlotSearch.Text;
+
+            sql1 = @"select OBJECTID as 'Id', Lot, Plot, [name_LAST] as 'Last Name', [name_FIRST] as 'First Name'
+                        from CemeteryPlotsVets where Lot = " + sqllot + " order by [name_LAST];";
+            // MessageBox.Show(sql1);
+            SqlDataAdapter da1 = new SqlDataAdapter(sql1, cnn);
+            da1.SelectCommand.CommandTimeout = 0;
+            DataSet ds1 = new DataSet();
+            da1.Fill(ds1);
+
+            SrcGrdList.DataContext = ds1.Tables[0];
+            SrcGrdList.SelectionMode = DataGridSelectionMode.Single;
+
+            da1.Dispose();
+            ds1.Dispose();
+
+        }
+
+        public class MapViewModel : INotifyPropertyChanged
+        {
+            public MapViewModel()
+            {
+                
+
+            }
+
+            private Map _map = new Map(Basemap.CreateStreets());
+
+            /// <summary>
+            /// Gets or sets the map
+            /// </summary>
+            public Map Map
+            {
+                get { return _map; }
+                set { _map = value; OnPropertyChanged(); }
+            }
+
+            /// <summary>
+            /// Raises the <see cref="MapViewModel.PropertyChanged" /> event
+            /// </summary>
+            /// <param name="propertyName">The name of the property that has changed</param>
+            protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                var propertyChangedHandler = PropertyChanged;
+                if (propertyChangedHandler != null)
+                    propertyChangedHandler(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+        }
+
     }
-
+   
 }
-
