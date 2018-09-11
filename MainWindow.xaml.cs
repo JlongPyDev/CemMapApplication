79cf5c5d6999d6220a0485fbcbaf7e237a86b1ef
JlongPyDev <jeffrey.long.88@gmail.com>
2018-09-11 10:48:46 -0400

    Latest Stable Version
    Still need to add legend functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,11 +20,15 @@ using System.Runtime.CompilerServices;
 using Esri.ArcGISRuntime.Data;
 using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Location;
-using Esri.ArcGISRuntime.Security;
 using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
+using Esri.ArcGISRuntime.LocalServices;
+using Esri.ArcGISRuntime.Portal;
 using Esri.ArcGISRuntime.UI;
+using System.Drawing;
+using Esri.ArcGISRuntime.Tasks;
+
 using System.ComponentModel;
+
 
 namespace CemMapApp
 {
@@ -39,7 +43,10 @@ namespace CemMapApp
 
         public MainWindow()
         {
-            //InitializeComponent();
+            InitializeComponent();
+
+            // Create the UI, setup the control references and execute initialization 
+            Initialize();
 
         }
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -63,7 +70,6 @@ namespace CemMapApp
                 MessageBox.Show("Logon failure.  Check Logon Id and Password.");
             }
         }
-
 
 
 
@@ -111,39 +117,191 @@ namespace CemMapApp
 
         }
 
-        public class MapViewModel : INotifyPropertyChanged
+              
+        private async void Initialize()
         {
-            public MapViewModel()
+            //Map myMap = new Map(Basemap.CreateTerrainWithLabels());
+            
+            
+            
+
+            string name = "Legend";
+
+
+            //LegendInfo MapLegend = new LegendInfo {Name, Symbol };
+
+            // TEST GDB SERVICE VS MAP SERVICE
+            // http://apexgis:6080/arcgis/rest/services/CemeteryHost/CEMTESTSERV/GeoDataServer/versions/dbo.DEFAULT
+            // WFS serv http://apexgis:6080/arcgis/services/CemeteryHost/CEMTESTSERV/GeoDataServer/WFSServer
+
+
+            // LEGEND ILayerContent
+
+            //var CemLot = new Uri("http://apexgis.ci.apex.nc.us:6080/arcgis/services/CemeteryHost/CemAppData/GeoDataServer");
+            var CemLot = new Uri("http://apexgis:6080/arcgis/rest/services/CemeteryHost/CEMTESTSERV/FeatureServer/0");
+            FeatureLayer LotLayer = new FeatureLayer(CemLot);
+
+
+
+            SimpleLineSymbol LotLineSymb = new SimpleLineSymbol(
+                SimpleLineSymbolStyle.Solid, System.Drawing.Color.Black, 1);
+
+            SimpleFillSymbol lotSymbol = new SimpleFillSymbol()
             {
-                
+                Color = System.Drawing.Color.Transparent,
+                Outline = LotLineSymb,
 
-            }
+            };
 
-            private Map _map = new Map(Basemap.CreateStreets());
+            SimpleRenderer LotRenderer = new SimpleRenderer(lotSymbol);
 
-            /// <summary>
-            /// Gets or sets the map
-            /// </summary>
-            public Map Map
-            {
-                get { return _map; }
-                set { _map = value; OnPropertyChanged(); }
-            }
+                // Lot label info
+            StringBuilder LotStringLabelBuild = new StringBuilder();
 
-            /// <summary>
-            /// Raises the <see cref="MapViewModel.PropertyChanged" /> event
-            /// </summary>
-            /// <param name="propertyName">The name of the property that has changed</param>
-            protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-            {
-                var propertyChangedHandler = PropertyChanged;
-                if (propertyChangedHandler != null)
-                    propertyChangedHandler(this, new PropertyChangedEventArgs(propertyName));
-            }
+            LotStringLabelBuild.AppendLine("{");
 
-            public event PropertyChangedEventHandler PropertyChanged;
-        }
+            LotStringLabelBuild.AppendLine("\"labelExpressionInfo\": {");
+            LotStringLabelBuild.AppendLine("\"expression\": \"return $feature.LOT_ID;\"},");
+            LotStringLabelBuild.AppendLine("\"maxScale\": 50,");
+            LotStringLabelBuild.AppendLine("\"minScale\": 1000,");
+            //     Align labels above the center of each point
+            LotStringLabelBuild.AppendLine("\"labelPlacement\": \"esriServerPolygonPlacementAlwaysHorizontal\",");
+            //     Use a white bold text symbol
+            LotStringLabelBuild.AppendLine("\"symbol\": {");
+            // Color ARGB  [R  ALPHA  B  G]
+            LotStringLabelBuild.AppendLine("\"color\": [13,38,68,255],");
+            LotStringLabelBuild.AppendLine("\"font\": {\"size\": 9, \"weight\": \"bold\"},");
+            LotStringLabelBuild.AppendLine("\"type\": \"esriTS\"}");
+            
+            LotStringLabelBuild.AppendLine("}");
 
-    }
-   
+            var LotLabelsJson = LotStringLabelBuild.ToString();
+
+            LabelDefinition LotLabelsDef = LabelDefinition.FromJson(LotLabelsJson);
+         
+            LotLayer.LabelDefinitions.Clear();
+
+            LotLayer.LabelDefinitions.Add(LotLabelsDef);
+            LotLayer.LabelsEnabled = true;
+
+                // CemPlot Properties
+            var CemPlot = new Uri("http://apexgis:6080/arcgis/rest/services/CemeteryHost/CEMTESTSERV/FeatureServer/1");
+            
+            FeatureLayer PlotLayer = new FeatureLayer(CemPlot);
+
+            var CemPlot2 = new Uri("http://apexgis:6080/arcgis/rest/services/CemeteryHost/CEMTESTSERV/FeatureServer/1");
+
+            FeatureLayer PlotLayer2 = new FeatureLayer(CemPlot);
+
+
+                // Plot Label Properties
+
+            StringBuilder PlotStringLabelBuild = new StringBuilder();
+
+            PlotStringLabelBuild.AppendLine("{");
+
+            PlotStringLabelBuild.AppendLine("\"labelExpressionInfo\": {");
+            PlotStringLabelBuild.AppendLine("\"expression\": \"return $feature.plot;\"},");
+            PlotStringLabelBuild.AppendLine("\"maxScale\": 10,");
+            PlotStringLabelBuild.AppendLine("\"minScale\": 250,");
+                //     Align labels above the center of each point
+            PlotStringLabelBuild.AppendLine("\"labelPlacement\": \"esriServerPolygonPlacementAlwaysHorizontal\",");
+                //     Use a white bold text symbol
+            PlotStringLabelBuild.AppendLine("\"symbol\": {");
+                // Color ARGB  [R  ALPHA  B  G]
+            PlotStringLabelBuild.AppendLine("\"color\": [13,38,68,255],");
+            PlotStringLabelBuild.AppendLine("\"font\": {\"size\": 8, \"weight\": \"italic\"},");
+            PlotStringLabelBuild.AppendLine("\"type\": \"esriTS\"}");
+
+            PlotStringLabelBuild.AppendLine("}");
+
+            var PlotLabelsJson = PlotStringLabelBuild.ToString();
+
+            LabelDefinition PlotLabelsDef = LabelDefinition.FromJson(PlotLabelsJson);
+
+            PlotLayer.LabelDefinitions.Clear();
+
+            PlotLayer.LabelDefinitions.Add(PlotLabelsDef);
+            PlotLayer.LabelsEnabled = true;
+
+                // Plot Symbology 
+            
+            var plotRenderer2 = new UniqueValueRenderer();
+
+            plotRenderer2.FieldNames.Add("name_LAST");
+
+            var plotOutline = new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, System.Drawing.Color.White, 1);
+                     
+            var town_plot = new SimpleFillSymbol(SimpleFillSymbolStyle.Solid, System.Drawing.Color.Pink, plotOutline);
+            plotRenderer2.UniqueValues.Add(new UniqueValue("Town Own", "Town Own", town_plot, "APEX"));
+            plotRenderer2.UniqueValues.Add(new UniqueValue("Town Own", "Town Own", town_plot, "TOWN OF APEX"));
+
+
+            var defaultFillSymbol2 = new SimpleFillSymbol(SimpleFillSymbolStyle.Null, System.Drawing.Color.Transparent, plotOutline);
+            plotRenderer2.DefaultSymbol = defaultFillSymbol2;
+
+            //plotRenderer.SceneProperties
+            var plotRenderer = new UniqueValueRenderer();
+
+            plotRenderer.FieldNames.Add("Plot_status");
+            
+
+            var validPlot = new SimpleFillSymbol(SimpleFillSymbolStyle.Solid, System.Drawing.Color.GreenYellow, plotOutline);
+            var unvalidPlot = new SimpleFillSymbol(SimpleFillSymbolStyle.Solid, System.Drawing.Color.Yellow, plotOutline);
+            var notsaleable = new SimpleFillSymbol(SimpleFillSymbolStyle.Solid, System.Drawing.Color.Red, plotOutline);
+                                
+            plotRenderer.UniqueValues.Add(new UniqueValue("Valid Plot","Valid Plot", validPlot, "Deeded"));
+            plotRenderer.UniqueValues.Add(new UniqueValue("Unvalid", "Unvalid Plot", unvalidPlot, "Unknown"));
+            plotRenderer.UniqueValues.Add(new UniqueValue("No Sale", "No Sell", notsaleable, "Not Saleable"));
+            
+            var defaultFillSymbol = new SimpleFillSymbol(SimpleFillSymbolStyle.Null, System.Drawing.Color.Transparent, plotOutline);
+          
+            plotRenderer.DefaultSymbol = defaultFillSymbol;
+            plotRenderer.DefaultLabel = "Other";
+
+            // CemBoundary Graphics
+
+            var CemBound = new Uri("http://apexgis:6080/arcgis/rest/services/CemeteryHost/CEMTESTSERV/FeatureServer/2");
+            FeatureLayer BoundLayer = new FeatureLayer(CemBound);
+
+            SimpleLineSymbol BoundLineSymb = new SimpleLineSymbol(
+                SimpleLineSymbolStyle.Solid, System.Drawing.Color.IndianRed, 3);
+
+            SimpleFillSymbol symbol = new SimpleFillSymbol()
+            {                
+                Outline = BoundLineSymb,              
+            };
+
+            SimpleRenderer renderer = new SimpleRenderer(symbol);
+
+            // Main GIS Operation Calls 
+
+
+            await PlotLayer.LoadAsync();
+            await PlotLayer2.LoadAsync();
+            await BoundLayer.LoadAsync();
+            await LotLayer.LoadAsync();
+
+            BoundLayer.Renderer = renderer;          
+            PlotLayer.Renderer = plotRenderer;
+            LotLayer.Renderer = LotRenderer;
+            PlotLayer2.Renderer = plotRenderer2;
+
+            Map myMap = new Map(BasemapType.ImageryWithLabelsVector, 35.728990, -78.856027, 18);
+
+            myMap.OperationalLayers.Add(BoundLayer);           
+            myMap.OperationalLayers.Add(PlotLayer);
+            myMap.OperationalLayers.Add(PlotLayer2);
+            myMap.OperationalLayers.Add(LotLayer);
+
+            // Assign the map to the MapView
+            myMap.MinScale = 2250;
+            myMap.MaxScale =30;
+            
+            MyMapView.Map = myMap;
+
+        } 
+
+    } 
+    
 }
